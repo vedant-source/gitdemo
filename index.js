3c923930ebbc1b21cadcbe98643cb5670a331917
Vedant Palwankar <64304187+vedant-source@users.noreply.github.com>
2022-08-04 15:29:51 +0530

    Add files via upload
    tested commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// console.log("Vedant");
+
+//push method
+
+// let animals = ["cat", "rat", "dog"];
+// console.log(animals);
+// let cnt = animals.push("horse", "pigs");
+// console.log(animals);
+// console.log(cnt);
+
+// Array Sorting
+
+// let arr = [77, 43, 5, 67, 99, 3];
+
+// console.log(
+//   arr.sort((a, b) => {
+//     a - b;
+//   })
+// );
+
+// let months = [
+//   "Jan",
+//   "Feb",
+//   "Mar",
+//   "Apr",
+//   "May",
+//   "Jun",
+//   "Jul",
+//   "Aug",
+//   "Sep",
+//   "Oct",
+//   "Nov",
+//   "Dec",
+// ];
+// console.log(months);
+
+// months.pop();
+// months.shift();
+// console.log(months);
+
+//splice method
+
+// var months = ["jan", "feb", "march"];
+
+// var newMonths = months.splice(2, 1, "april");
+// console.log(months);
+
+// var months = ["jan", "feb", "march"];
+
+// var updateMonth = months.splice(2, 1, "MARCH");
+// console.log(months);
+// console.log(updateMonth);
+
+//map
+
+// let arr = [25, 36, 49, 64, 81];
+
+// let arrSqr = arr.map((curEle) => {
+//   return Math.sqrt(curEle);
+// });
+
+// console.log(arrSqr);
+
+// let arr = [2, 3, 4, 5, 6, 7, 8];
+
+// let arr2 = arr
+//   .map((curEle) => {
+//     return curEle * 2;
+//   })
+//   .filter((curEle) => {
+//     return curEle > 10;
+//   });
+
+// console.log(arr2);
